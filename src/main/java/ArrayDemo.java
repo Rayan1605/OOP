3e3561c18452729b3c3920eaf07e376f0df952d4
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-01-29 19:02:32 +0000

    Fixing the bug

--- src/main/java/ArrayDemo.java
+++ src/main/java/ArrayDemo.java
@@ -4,20 +4,17 @@ public class ArrayDemo {
     public static void main(String[] args) {
         Scanner myinput = new Scanner(System.in);
         int[] array = new int[10];
-        array[0] = 100;
-        array[1] = 200;
-        array[2] = 300;
-        array[3] = 400;
-        array[4] = 500;
-        array[5] = 600;
-        array[6] = 700;
-        array[7] = 800;
-        array[8] = 900;
-        array[9] = 1000;
-
-
+        int hundred   = 100;
+        int counter = 0;
         for (int i = 0; i < array.length; i++) {
-            System.out.println("In index " + i + " of the array contain the number " + array[i] + " \n");
+            array[i] = hundred;
+            hundred = hundred+100;
+        }
+
+        for (int x : array) {
+            System.out.println("In the index " + counter + ", it contains the number " + x +" \n");
+            counter++;
+
         }
         int a = 10;
         do {
